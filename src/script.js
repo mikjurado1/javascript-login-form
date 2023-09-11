f24f047dd650a96a3fcb8184bc1284affb14e04c
Michael Jurado <mikjurado1@gmail.com>
2023-09-11 20:11:15 +0200

    Validate User Credentials & show validation status message

--- src/script.js
+++ src/script.js
@@ -30,4 +30,9 @@ addEventListener("submit", (event) => {
     2. If they are, call renderSuccess()
     3. If they are not, call renderError()
    */
+
+  const isValidUser = usersTable.some(
+    (user) => (user.username == email) && (user.password == password)
+  );
+  return isValidUser ? renderSuccess() : renderError();
 });
